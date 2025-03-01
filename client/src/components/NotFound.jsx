769e30794835b48163e0ef201b8157affe8b392a
Bibash45 <bibashcdry46@gmail.com>
2025-03-02 02:20:24 +0545

    small changes on not found page

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import not from "../../src/assets/Images/Not Found Image/not.gif"
 
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-primary-600 to-indigo-800">
       <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg ">
         <div className="flex flex-col items-center mb-6">
+          <img src={not} alt="" />
           <h1 className="text-6xl font-bold text-gray-800 mb-2 animate-pulse">404</h1>
           <p className="text-2xl text-gray-600 animate-bounce">Page Not Found</p>
         </div>
